15ce5ff66aff2bd3766c49634e82561f3713e488
asliddin715 <karomatovasliddin715@gmail.com>
2024-07-23 09:55:49 +0500

    lesson2

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,7 +1,25 @@
+
+import { useRef } from "react";
+
 export default function Dashboard() {
+  const inputRef = useRef();
+
   return (
-    <div>
-      <h2>Dashboard page</h2>
-    </div>
+    <form className='addForm'>
+      <label htmlFor='addItem'>Add Item</label>
+      <input
+        autoFocus
+        ref={inputRef}
+        id='addItem'
+        type='text'
+        placeholder='Add Item'
+        required
+      />
+      <button type='submit' aria-label='Add Item'>
+      Add Todo
+      </button>
+    </form>
   );
-}
+};
+
+
